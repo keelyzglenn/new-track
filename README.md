d11359ffe1d1297336448bc9fb7ad0ef157b2941
keely glenn <glenj392@newschool.edu>
2017-01-13 13:20:28 -0800

    add README.md file to folder

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Keely Silva Glenn Track Suggester_
+
+#### _Integration of Javascript, HTML, and CSS  01.06.2017_
+
+#### By _**Keely Silva-Glenn**_
+
+## Description
+
+_This project demonstrates my use of javascript to make a quiz which helps students decide which Epicodus track is best for them. This project also shows my ability to combine html and css with javascript._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open in web browser of your choice_
+* _Take the quiz to determine your track_
+
+## Access
+
+_https://github.com/keelyzglenn/portfolio_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_Bootstrap_
+_Javascript_
+_jQuery_
+
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2016 **_{Keey Silva-Glenn}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 // Backend
 
 
+
+
+
 // Frontend
 $(document).ready(function() {
+
+
   $("form#track").submit(function(event){
     event.preventDefault();
+
     var q1 = parseInt($("input:radio[name=q1]:checked").val());
     var q2 = parseInt($("input:radio[name=q2]:checked").val());
     var q3 = parseInt($("input:radio[name=q3]:checked").val());
     var q4 = parseInt($("input:radio[name=q4]:checked").val());
     var q5 = parseInt($("input:radio[name=q5]:checked").val());
     var track = q1 + q2 + q3 + q4 + q5 ;
+    var nameInput = $("input#name").val();
+
+    $(".result").text(nameInput);
 
     if (track >= 6 && track <= 9) {
       $(".ruby").show();
+
     } else if (track >= 10 && track <= 13) {
       $(".java").show();
+
     } else if (track >= 13 && track <= 16) {
       $(".cnet").show();
     }
-  });
-
-  $("form#userName").submit(function(event) {
-    var name = $("#name").val();
-    var result= "Hello " + name + " " + "based on your answers, your suggested track is...";
-    $(".result").text(result);
   });
 });
