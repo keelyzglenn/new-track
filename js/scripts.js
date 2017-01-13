181d4333120250a09d69f740090ebf94d62dc306
keely glenn <glenj392@newschool.edu>
2017-01-13 11:54:26 -0800

    add folders to the track folder

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,17 @@ $(document).ready(function() {
     var q3 = parseInt($("input:radio[name=q3]:checked").val());
     var q4 = parseInt($("input:radio[name=q4]:checked").val());
     var q5 = parseInt($("input:radio[name=q5]:checked").val());
-    var track= q1 + q2 + q3 + q4 + q5 ;
+    var track = q1 + q2 + q3 + q4 + q5 ;
 
-    if (track >= 5 && track <= 8) {
+    if (track >= 6 && track <= 9) {
       $(".ruby").show();
-      location.href="answers.html";
-    } else if (track >= 9 && track <= 11) {
-      $(".php").show();
-      location.href="answers.html";
-    } else if (track >= 12 && track <= 14) {
+      location.href="ruby.html"
+    } else if (track >= 10 && track <= 13) {
       $(".java").show();
-      location.href="answers.html";
-    } else if (track >= 14 && track <= 16) {
+      location.href="java.html";
+    } else if (track >= 13 && track <= 16) {
       $(".cnet").show();
-      location.href="answers.html";
+      location.href="c.html";
     }
   });
 });
